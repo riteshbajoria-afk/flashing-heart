abf00e4822433d643145e473801b818df3c4e4e8
riteshbajoria-afk <riteshbajoria@gmail.com>
2025-08-02 12:02:29 +0530

    Update main.blocks, main.ts, _history

--- main.ts
+++ main.ts
@@ -1,13 +1,1 @@
-input.onButtonPressed(Button.AB, function () {
-    led.plot(2, 4)
-    basic.showLeds(`
-        # # # # #
-        # . . . .
-        # . # # #
-        # . . . #
-        # # # # #
-        `)
-})
-basic.forever(function () {
-	
-})
+
